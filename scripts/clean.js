afca395e7c1081665acb2fe4778584ed4c0688d9
Ingruz <ingro85@gmail.com>
2016-10-10 21:36:23 +0200

    Add clean scripts to bootstrap

---
+++ scripts/clean.js
@@ -0,0 +1,21 @@
+const path    = require('path');
+const rimraf  = require('rimraf');
+
+function doClean(packageName, folderName) {
+    rimraf(path.join(process.cwd(), `./packages/${folderName}/node_modules/${packageName}`), err => {
+        if (err) {
+            return;
+        }
+
+        console.log(`Module ${packageName} cleaned from ${folderName}!`);
+    });
+}
+
+function cleanModule(packageName) {
+    doClean(packageName, 'steiner-demo');
+    doClean(packageName, 'steiner');
+}
+
+cleanModule('react');
+cleanModule('react-router');
+cleanModule('redux-form');

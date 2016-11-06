83efd8f34ed24790a4b04e809d5591602f505f9e
Ingruz <ingro85@gmail.com>
2016-11-06 16:39:00 +0100

    Added translations in settings state

--- packages/steiner/src/settings/actions.js
+++ packages/steiner/src/settings/actions.js
@@ -1,4 +1,5 @@
 export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
+export const SET_TRANSLATIONS = 'SET_TRANSLATIONS';
 
 export function updateSettings(settings) {
     return {
@@ -7,10 +8,19 @@ export function updateSettings(settings) {
     };
 }
 
+export function setTranslations(translations) {
+    return {
+        type: SET_TRANSLATIONS,
+        payload: translations
+    };
+}
+
 export const actions = {
-    updateSettings
+    updateSettings,
+    setTranslations
 };
 
 export const actionTypes = {
-    updateSettings: UPDATE_SETTINGS
+    updateSettings: UPDATE_SETTINGS,
+    setTranslations: SET_TRANSLATIONS
 };
--- packages/steiner/src/settings/reducer.js
+++ packages/steiner/src/settings/reducer.js
@@ -2,9 +2,11 @@ import Immutable from 'seamless-immutable';
 
 import { actionTypes } from './actions';
 import { createReducer } from '../helpers/reducerCreator';
+import messages from '../messages/en';
 
 export const DEFAULT_STATE = Immutable({
-    language: 'en'
+    language: 'en',
+    translations: messages
 });
 
 const handlers = {
@@ -13,6 +15,9 @@ const handlers = {
             ...state,
             ...action.payload
         });
+    },
+    [actionTypes.setTranslations]: (state, action) => {
+        return state.set('translations', action.payload);
     }
 };
 
@@ -20,4 +25,12 @@ export default createReducer(handlers, DEFAULT_STATE);
 
 export function getSettings(state) {
     return state.settings;
-}
+};
+
+export function getLanguage(state) {
+    return state.settings.language;
+};
+
+export function getTranslations(state) {
+    return state.settings.translations
+};

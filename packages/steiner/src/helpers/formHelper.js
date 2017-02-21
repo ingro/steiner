0e3e98239edfd4129db8d431a6122d0dbbfafe50
Emanuele Ingrosso <ingro85@gmail.com>
2017-02-21 14:42:02 +0100

    Allow the option to override the id key in create submit form helper

--- packages/steiner/src/helpers/formHelper.js
+++ packages/steiner/src/helpers/formHelper.js
@@ -1,10 +1,17 @@
 import { createFormAction } from './reduxFormSaga';
+import _ from 'lodash';
 
 const identity = data => data;
 
-export function createSubmit(actionTypes, dataTransformer = identity) {
+const optionsDefaults = {
+    idKey: 'id'
+};
+
+export function createSubmit(actionTypes, dataTransformer = identity, options = {}) {
+    _.defaults(options, optionsDefaults);
+
     return function submit(data, dispatch) {
-        const action = data.id
+        const action = data[options.idKey]
             ? createFormAction(actionTypes.update, [actionTypes.updateSuccess, actionTypes.updateFail])
             : createFormAction(actionTypes.create, [actionTypes.createSuccess, actionTypes.createFail]);
 
